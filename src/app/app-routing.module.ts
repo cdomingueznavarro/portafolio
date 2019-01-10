8b4a48421c71b813fd958932187cf3621bf0f039
Dominguez Navarro <cldoming@t-systems.es>
2019-01-10 14:31:27 +0100

    Template listo con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+// tslint:disable-next-line:quotemark
+import { NgModule } from "@angular/core";
+// tslint:disable-next-line:quotemark
+import { Routes, RouterModule } from "@angular/router";
+// tslint:disable-next-line: quotemark
+import { PortafolioComponent } from "./pages/portafolio/portafolio.component";
+// tslint:disable-next-line:quotemark
+import { AboutComponent } from "./pages/about/about.component";
+// tslint:disable-next-line: quotemark
+import { ItemComponent } from "./pages/item/item.component";
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  // tslint:disable-next-line:quotemark
+  { path: "home", component: PortafolioComponent },
+  { path: "about", component: AboutComponent },
+  // tslint:disable-next-line:quotemark
+  { path: "item", component: ItemComponent },
+  { path: "**", pathMatch: "full", redirectTo: "home" }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
